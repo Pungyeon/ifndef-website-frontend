02a7a2b5acd5772b1680b73ffd5bc6512f28e367
pungy <lja@credocom.dk>
2018-06-07 13:15:07 +0200

    added a few things, nothing of importance, mostly just testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'the ifndef_lja website! *WORK IN PROGRESS*';
 }
