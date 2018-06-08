49abec0f5defa0efd5e8a99d3b375442a3160ce8
pungy <lja@credocom.dk>
2018-06-09 00:33:49 +0200

    still working on cosmetics.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+/*
+ * TODO: Make the 'about me' section a side panel that can be hidden
+ * TODO: Create a search function. Just implement it in pure javascript, there is no need to implement anything more exciting, for now.
+ * TODO: Get data from MongoDB rather than from static numbers
+ * TODO: Fill descriptions of work 
+ * TODO: Add contact details
+ * TODO: Add links of articles
+ */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
