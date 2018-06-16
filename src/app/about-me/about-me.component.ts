af56a6f9aa62442113b06a9c9e81d081f497afad
Lasse Martin Jakobsen <lasse@tengen.dk>
2018-06-16 21:01:08 +0200

    oh shit, this is gonna be merge hell....

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -14,21 +14,37 @@ export class AboutMeComponent implements OnInit {
       Company: "Credocom A/S",
       Title: "Solutions Architect",
       Description: "Solutions Architect at Credocom A/S Specializing in data analysis and development of automation tools for data analysis, with focus on analysing network and security solutions.",
+      Projects: [
+        "Technical lead on PinPoint solution, enabling network engineers to easily retrieve and analyse packet captures from network recorders. The solution delivered using a cloud-native approach, to ensure scalability and agility in development.",
+        "Lead Developer on analysis tool for 802.1x deployment for large international organisation, to provide network engineers with data on client information and simplified management of clients. The tool assisting in a smooth deployment for over 40.000 clients.",
+        "Rewrite of legacy system responsible for invoicing of hosting customer, with focus on error handling and improving security implementation. Establishing confidence in the reliability of the system."
+      ]
     },
     {
       Company: "Credocom A/S",
       Title: "Consulant",
       Description: "Working as a Consultant at Credocom A/S, specializing in cloud technology, network analysis and development of software for deployment and analysis.",
+      Projects: [
+        "Lead Developer of monitoring and reporting solution for Riverbed Steelhead WAN optimisation appliances, to ensure performance stability in the event of failures or unexpected events.",
+        "Analysis of network traffic for a larger government organisation, troubleshooting slow network connections of the existing VDI solution. The analysis discovered several bottlenecks and protocol instabilities and actions taken, based on analysis, resulted in a decrease of response times by 60% on average."
+
+      ]
     },
     {
       Company: "Brdr. Hartmann",
       Title: "Cloud Technician",
       Description: "As Cloud Technician, I have the technical responsibilities for Office 365 and Azure in Brdr. Hartmann. I also assist front -and back-end support for our 500+ IT users.",
+      Projects: [
+        "Led a cloud infrastructure migration that successfully migrated 1500 users to Office 365 in 4 weeks. The project was successful due to rigorous planning and development of migration tools with Powershell."
+      ]
     },
     {
       Company: "Brdr. Hartmann",
       Title: "Student",
       Description: "Currently responsible for Office 365 Research and Development, Assistent for front-end & back-end support.",
+      Projects: [
+        "Developer for logistic forecast reporting project. Lasse automated many manual reporting and presentation processes that enabled more frequent and accurate forecasts."
+      ]
     }
   ]
 
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -15,11 +15,19 @@ export class ArticlesComponent implements OnInit {
   details = [ 1, 2, 3,4 ];
 
   articles = [
-    { Title: "How to Containerise your Nodejs Application", Description: "Short introduction to Dockerising Nodejs application",
+    {
+      Title: "Implementing Authentication for Golang Gin API, using Auth0",
+      Likes: 0, Date: "2018-08-20",
+      Tags: ["golang", "auth0"],
+      Image: ["/assets/placeholder.png"],
+      MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/vim-setup/master/README.md"
+    },
+    { 
+      Title: "Securing Microservices using an API Gateway", Description: "How to secure your microservices with authentication, load balancing and encryption using NGINX as an API Gateway",
       Likes: 2, Date: "2018-08-20",
-      Tags: ["docker", "nodejs"],
-      Image: "https://www.fullstackpython.com/img/logos/docker-wide.png",
-      MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/docker-nginx-example/master/README.md"
+      Tags: ["nginx", "api gateway"],
+      Image: "https://cdn-1.wp.nginx.com/wp-content/uploads/2015/04/NGINX_logo_rgb-01.png",
+      MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/docker-example/master/README.md"
     },
     { Title: "Introduction to RabbitMQ using Golang and Docker", Description: "Brief introduciton to using RabbitMQ with Golang", 
       Likes: 2, Date: "2018-08-20",
@@ -27,19 +35,18 @@ export class ArticlesComponent implements OnInit {
       Image: "https://sciencelogic.com/wp-content/uploads/2018/01/logo-rabbitmq.png",
       MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/go-rabbitmq-example/master/README.md"
     },
-    { Title: "Securing Microservices using an API Gateway", Description: "How to secure your microservices with authentication, load balancing and encryption using NGINX as an API Gateway",
-      Likes: 2, Date: "2018-08-20",
-      Tags: ["nginx", "api gateway"],
-      Image: "https://cdn-1.wp.nginx.com/wp-content/uploads/2015/04/NGINX_logo_rgb-01.png",
-      MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/docker-example/master/README.md"
-    },
     { Title: "Using Traefik as a Load Balancer for Docker Compose", Description: "How to secure your microservices with authentication, load balancing and encryption using NGINX as an API Gateway",
       Likes: 4, Date: "2018-08-20",
       Tags: ["traefik", "docker"],
       Image: "https://d33wubrfki0l68.cloudfront.net/e29410f43273a18d40e1bd6e41641f5afdfc8057/eb7ca/traefik.logo.png",
       MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/docker-traefik-example/master/README.md"
     },
-      
+    { Title: "How to Containerise your Nodejs Application", Description: "Short introduction to Dockerising Nodejs application",
+      Likes: 2, Date: "2018-08-20",
+      Tags: ["docker", "nodejs"],
+      Image: "https://www.fullstackpython.com/img/logos/docker-wide.png",
+      MarkdownLink: "https://raw.githubusercontent.com/Pungyeon/docker-nginx-example/master/README.md"
+    } 
   ]
 
   onArticleSelect(article: any) {
