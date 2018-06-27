aaf8b3bbaddcecc08a2b3fa44d2f067516f408bd
Lasse Martin Jakobsen <lasse@tengen.dk>
2018-06-27 08:49:51 +0200

    removed mat-icon from app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import {
-  MatButtonModule, MatCardModule,
-  MatIconModule, MatDividerModule, MatToolbarModule, MatMenuModule } from '@angular/material';
+  MatButtonModule, MatCardModule, MatDividerModule,
+  MatToolbarModule, MatMenuModule } from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { StateService } from 'src/app/service/state.service';
 import { AppComponent } from './app.component';
@@ -31,7 +31,6 @@ import { ErrorComponent } from './error/error.component';
     HttpClientModule, // ?? maybe possible to exclude this
     MatCardModule, // EXCLUDE - FrontPage & Menu
     FlexLayoutModule, // EXCLUDE - FrontPage & Menu
-    MatIconModule, // INCLUDE - about site and me, article
     MatDividerModule,
     MatButtonModule
   ],
