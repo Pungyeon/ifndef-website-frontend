63c46e47fcfa7ab698ba8fe4e972c18e1bd959ab
Lasse Martin Jakobsen <lasse@tengen.dk>
2018-06-27 08:38:25 +0200

    removed lazy loading for toolbar, it made no sense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {
-  MatButtonModule, MatCardModule,
-  MatIconModule, MatDividerModule } from '@angular/material';
-import { FlexLayoutModule } from "@angular/flex-layout";
-
-import { AppComponent } from './app.component';
-import { ArticlesComponent } from './articles/articles.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import {
+  MatButtonModule, MatCardModule,
+  MatIconModule, MatDividerModule, MatToolbarModule, MatMenuModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { StateService } from 'src/app/service/state.service';
+import { AppComponent } from './app.component';
+
+import { ArticlesComponent } from './articles/articles.component';
 import { AboutSiteComponent } from './about-site/about-site.component';
 import { ErrorComponent } from './error/error.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticlesComponent, // mat-card, flexbox
     AboutSiteComponent, // mat-card, flexbox, 
-    ErrorComponent // mat-card, flexbox
+    ErrorComponent, // mat-card, flexbox
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatButtonModule, // is used by all
+    MatToolbarModule, // is used by all
+    MatMenuModule, // is used by all
     HttpClientModule, // ?? maybe possible to exclude this
     MatCardModule, // EXCLUDE - FrontPage & Menu
     FlexLayoutModule, // EXCLUDE - FrontPage & Menu
     MatIconModule, // INCLUDE - about site and me, article
-    MatDividerModule
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [StateService],
   bootstrap: [AppComponent]
